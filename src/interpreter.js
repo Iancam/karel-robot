1db6211bb86b18977fabcda9fd62068203949f0c
Ian Campbell <ian.campbell.92@gmail.com>
2020-10-27 15:06:43 -0600

    interpreter accepts globals, i.e. python `range`

--- src/interpreter.js
+++ src/interpreter.js
@@ -1,16 +1,13 @@
-// import interfaceFactory from './karelInterface';
-// import karelModelFactory from './karelModel';
-
+// because this function uses a 'with' statement,
+// this code cannot be imported in the usual way,
+// and has to come from global scope
 /**
  *
  * @param {string} code
  * @param {import('./karelModel').karelState} initialWorld
  */
-function _runCode(code, karelInterface) {
-  with ({ ...karelInterface, code }) {
+function _runCode(code, karelInterface, globals) {
+  with ({ ...karelInterface, ...globals, code }) {
     eval(code + '\nmain()');
   }
 }
-
-// const diffs = runCode(code);
-// console.log(diffs);
