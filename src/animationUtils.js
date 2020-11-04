6cf16613d90da40549ecce5d1b3a13568b4372b3
Ian Campbell <ian.campbell.92@gmail.com>
2020-11-03 20:56:04 -0700

    animation refactor

---
+++ src/animationUtils.js
@@ -0,0 +1,61 @@
+export const updateStatery = (states, diffs, onUpdate, diffOffset = -1) => {
+  return {
+    indexes: states.length,
+    updateState: index => {
+      console.log(index, states.length);
+      if (!states[index]) return false;
+      onUpdate(
+        states[index],
+        diffs[index + diffOffset],
+        index,
+        index + diffOffset
+      );
+      return true;
+    },
+  };
+};
+
+export const animatery = (speed, update) => {
+  let index = 0;
+  let last;
+  let animating = true;
+  return {
+    reset: () => {
+      update(0);
+      animating = false;
+    },
+    speed: newSpeed => (newSpeed ? (speed = newSpeed) : speed),
+    index: newIndex => {
+      if (newIndex) {
+        animating = false;
+        update(newIndex);
+      }
+      return newIndex ? (speed = newIndex) : index;
+    },
+
+    animate: () => {
+      const animator = ts => {
+        if (!animating) return false;
+        if (last === undefined) last = ts;
+        const timeDiff = ts - last;
+        if (timeDiff > speed) {
+          if (!update(index++)) return;
+          last = ts;
+        }
+        requestAnimationFrame(animator);
+      };
+      requestAnimationFrame(animator);
+    },
+  };
+};
+
+export const makeUpdator = (func, name, initValue) => {
+  let oldVal = initValue;
+  return function (arg) {
+    if (!arg) return func();
+
+    func(arg);
+    this.requestUpdate(name, oldVal);
+    oldVal = arg;
+  };
+};
