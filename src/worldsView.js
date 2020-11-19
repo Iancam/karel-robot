fb9fbe5305097c4073e84e43bb81ac0661db8672
Ian Campbell <ian.campbell.92@gmail.com>
2020-11-19 14:21:19 -0700

    adding class for styling, named parameters

--- src/worldsView.js
+++ src/worldsView.js
@@ -1,9 +1,13 @@
 // worldsView.js;
 import { html } from 'lit-element';
 
-export function worldsView(worlds, onSelect, selected) {
+export function worldsView({ worlds, onSelect, selected, className }) {
   return html`
-    <select name="world" @change=${e => onSelect(e.target.value)}>
+    <select
+      class=${className}
+      name="world"
+      @change=${e => onSelect(e.target.value)}
+    >
       ${worlds.map(
         ({ name }) =>
           html`<option ?selected=${selected === name} value=${name}>
